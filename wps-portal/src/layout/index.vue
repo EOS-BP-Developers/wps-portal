5526f9608ff63bf090b0cb8dc4982d84b2679b62
Hugo <campanella.hugo@hotmail.fr>
2018-07-18 14:12:02 +0000

    vue cli

---
+++ wps-portal/src/layout/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <AppHeader></AppHeader>
+    <div class="wrapper">
+      <transition name="fade">
+        <router-view :key="$route.fullPath"></router-view>
+      </transition>
+    </div>
+    <AppFooter></AppFooter>
+  </div>
+
+</template>
+
+<script>
+  import AppHeader from './Header.vue'
+  import AppFooter from './Footer.vue'
+
+  export default {
+    name: 'AppLayout',
+    components: {
+      AppHeader,
+      AppFooter
+    }
+  }
+</script>
+
+<style>
+#app {
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+}
+
+.wrapper {
+  flex: 1;
+}
+</style>
