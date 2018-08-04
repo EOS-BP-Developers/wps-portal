09371bf700fa002c555106ce7a7dbdd6bcc5b4d5
Adrian Hunter <adrian@saltblock.io>
2018-08-04 17:45:02 +0200

    heroku setup

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const serveStatic = require('serve-static');
 let app = express();
 app.use(serveStatic(__dirname + "/dist"));
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log('Listening on port ' + port)
 });
