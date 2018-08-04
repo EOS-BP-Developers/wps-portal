7b1da75bcf683cfd54c3b07600f36a49b3ff8a58
Hugo <hg.campanella@gmail.com>
2018-08-05 01:12:26 +0200

    Create Project Page

--- src/pages/Category.vue
+++ src/pages/Category.vue
@@ -15,43 +15,74 @@
   <!-- Start Tabs Container -->
   <div class="container" style="margin-top:0.5em;">
     <!-- Start of Categories Tabs -->
-    <b-tabs>
-      <!-- Popular Projects Tab -->
-      <b-tab title="Popular" active>
+          <b-card no-body>
+            <b-tabs card>
 
-        <CategoryTabContent></CategoryTabContent>
+              <!-- Hot Tab - Most voted projects on top -->
+              <b-tab title="Hot" active>
 
-      <!-- End of Popular Tab -->
-      </b-tab>
-      <!-- Community Tab -->
-      <b-tab title="Community" >
+                <!-- Projects cards container -->
+                <div class="container">
+                  <div class="row">
+                    
+                    <!-- Card column -->
+                    <div class="col-md-6 border" v-if="item.category === 'Popular'" v-for="item in projects" style="margin-top:1em;">
+                      <div class="container">
+                        <div class="row" >
+                          <div class="col-md-6">
+                            <img v-bind:src="item.image">
+                          </div>
+                          <div class="col-md-6">
+                            <div class="container fluid">
+                              <div class="row">
+                                <div class="col-md-6" style="margin-top:0.5em;">
+                                  <p style="color:green;"> {{ item.status }} </p>
+                                </div>
+                                <div class="col-md-6" style="margin-top:0.5em;">
+                                  <p> {{ item.count }} </p>
+                                </div>
+                              </div>
+                            </div>
 
-        <CategoryTabContent></CategoryTabContent>
+                            <h2>{{ item.name }}</h2>
+                            <p>{{ item.description }}</p>
 
-      <!-- End of Community Tab -->
-      </b-tab>
-      <!-- Education tab Content -->
-      <b-tab title="Education">
+                            <div class="container fluid">
+                              <div class="row">
+                                <div class="col-md-6">
+                                  <p>{{ item.votes }}</p>
+                                </div>
+                                <div class="col-md-6">
+                                  <span class="badge badge-secondary">{{ item.category }}</span>
+                                </div>
+                              </div>
+                            </div>
 
-        <CategoryTabContent></CategoryTabContent>
+                          </div>
+                        </div>
+                      </div>
+                    <!-- End of card column -->
+                    </div>
 
-      </b-tab>
-      <b-tab title="Entertainment">
+                  <!-- End or Row -->
+                  </div>
+                </div>
 
-        <CategoryTabContent></CategoryTabContent>
+              <!-- Latest Tab - Most Recent projects on top -->
+              </b-tab>
+              <b-tab title="Latest">
 
-      </b-tab>
-      <b-tab title="Games">
+                <div class="container">
+                  <div class="row">
 
-        <CategoryTabContent></CategoryTabContent>
+                    <!-- Need same cards ordered by date -->
 
-      </b-tab>
-      <b-tab title="Other">
+                  </div>
+                </div>
 
-        <CategoryTabContent></CategoryTabContent>
-
-      </b-tab>
-    </b-tabs>
+              </b-tab>
+            </b-tabs>
+          </b-card>
 
     <!-- Load more Button -->
     <div class="container" style="margin-top:1em; margin-bottom:1em;">
@@ -79,10 +110,31 @@ export default {
   data () {
     return {
       page: 'Category Name',
-      subtitle: 'Explore the promising game development projects which need your support'
+      subtitle: 'Explore the promising game development projects which need your support',
+      statistics: {
+        'Total Voters': '183,434',
+        'Total Projects': '143,432',
+        'Funded Projects': '734,345',
+        'Ongoing Projects': '235,289'
+      },
+      projects: [
+        { image: 'https://vignette.wikia.nocookie.net/undertale-rho/images/5/5f/Placeholder.jpg/revision/latest?cb=20180213155916', status: 'Ongoing', count: '12', name: 'Warhammer', description: 'This is a project description example to illustrate', votes: '432', category: 'Popular' },
+        { image: 'https://vignette.wikia.nocookie.net/undertale-rho/images/5/5f/Placeholder.jpg/revision/latest?cb=20180213155916', status: 'Ongoing', count: '10', name: 'Red Alert', description: 'This is a project description example to illustrate', votes: '432', category: 'Popular' },
+        { image: 'https://vignette.wikia.nocookie.net/undertale-rho/images/5/5f/Placeholder.jpg/revision/latest?cb=20180213155916', status: 'Ongoing', count: '19', name: 'GTA', description: 'This is a project description example to illustrate', votes: '432', category: 'Community' },
+        { image: 'https://vignette.wikia.nocookie.net/undertale-rho/images/5/5f/Placeholder.jpg/revision/latest?cb=20180213155916', status: 'Ongoing', count: '4', name: 'Diablo', description: 'This is a project description example to illustrate', votes: '432', category: 'Popular' },
+        { image: 'https://vignette.wikia.nocookie.net/undertale-rho/images/5/5f/Placeholder.jpg/revision/latest?cb=20180213155916', status: 'Ongoing', count: '19', name: 'Sim City', description: 'This is a project description example to illustrate', votes: '432', category: 'Community' },
+        { image: 'https://vignette.wikia.nocookie.net/undertale-rho/images/5/5f/Placeholder.jpg/revision/latest?cb=20180213155916', status: 'Ongoing', count: '10', name: 'Red Alert', description: 'This is a project description example to illustrate', votes: '432', category: 'Popular' },
+        { image: 'https://vignette.wikia.nocookie.net/undertale-rho/images/5/5f/Placeholder.jpg/revision/latest?cb=20180213155916', status: 'Ongoing', count: '19', name: 'GTA', description: 'This is a project description example to illustrate', votes: '432', category: 'Community' },
+        { image: 'https://vignette.wikia.nocookie.net/undertale-rho/images/5/5f/Placeholder.jpg/revision/latest?cb=20180213155916', status: 'Ongoing', count: '4', name: 'Diablo', description: 'This is a project description example to illustrate', votes: '432', category: 'Popular' },
+        { image: 'https://vignette.wikia.nocookie.net/undertale-rho/images/5/5f/Placeholder.jpg/revision/latest?cb=20180213155916', status: 'Ongoing', count: '19', name: 'Sim City', description: 'This is a project description example to illustrate', votes: '432', category: 'Community' },
+        { image: 'https://vignette.wikia.nocookie.net/undertale-rho/images/5/5f/Placeholder.jpg/revision/latest?cb=20180213155916', status: 'Ongoing', count: '19', name: 'GTA', description: 'This is a project description example to illustrate', votes: '432', category: 'Community' },
+        { image: 'https://vignette.wikia.nocookie.net/undertale-rho/images/5/5f/Placeholder.jpg/revision/latest?cb=20180213155916', status: 'Ongoing', count: '13', name: 'Project Name', description: 'This is a project description example to illustrate', votes: '432', category: 'Popular' },
+        { image: 'https://vignette.wikia.nocookie.net/undertale-rho/images/5/5f/Placeholder.jpg/revision/latest?cb=20180213155916', status: 'Ongoing', count: '19', name: 'GTA', description: 'This is a project description example to illustrate', votes: '432', category: 'Community' }
+      ],
     }
-  }
+  },
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/pages/CreateProject.vue
+++ src/pages/CreateProject.vue
@@ -1,9 +1,173 @@
 <template>
-  <div>
-    <h1 style="padding-top:330px;padding-bottom:330px;">
-      {{ page }}
-    </h1>
-  </div>
+  <main role="main">
+        <!-- Page Title Jubotron -->
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+          <h1 class="display-6">
+            {{ title }}
+          </h1>
+          <p>
+          {{ subtitle }}
+          </p>
+      </div>
+    <!-- End of jumbotron -->
+    </div>
+
+    <!-- Project Creation Form -->
+    <div class="container">
+      <b-form @submit="onSubmit" @reset="onReset" v-if="show" style="text-align:left;">
+          
+        <h3>Main information</h3>
+
+        <b-form-group id="categories"
+                      label="Select Project Category"
+                      label-for="categories">
+          <b-form-select id="categories"
+                        :options="categories"
+                        required                        
+                        v-model="form.categories">
+          </b-form-select>
+        </b-form-group>
+
+        <b-form-group id="title"
+                      label="Title"
+                      label-for="title"
+                      description="Maximum 60 characters">
+          <b-form-input id="title"
+                        type="text"
+                        v-model="form.title"
+                        required
+                        placeholder="Type Project Title"
+                        :maxlength="maxTitle">
+          </b-form-input>
+        </b-form-group>
+
+        <b-form-group id="subtitle"
+                      label="Subitle"
+                      label-for="subtitle"
+                      description="Maximum 140 characters">
+          <b-form-input id="subtitle"
+                        type="text"
+                        v-model="form.subtitle"
+                        required
+                        placeholder="Type Project Subtitle"
+                        :maxlength="maxSubtitle">
+          </b-form-input>
+        </b-form-group>
+
+        <b-form-group id="media"
+                      label="Image or Video"
+                      label-for="media">
+          <b-form-input id="media"
+                        type="url"
+                        v-model="form.media"
+                        required
+                        placeholder="Copy Paste an Image or Video URL to illustrate project">
+          </b-form-input>
+        </b-form-group>
+
+        <h3>Project Description</h3>
+
+        <b-form-group id="overview"
+                      label="Project Overview"
+                      label-for="overview">
+          <b-form-textarea id="overview"
+                        type="text"
+                        v-model="form.overview"
+                        required
+                        placeholder="Describe your porject to the community, your story will help us to understand why you started this project and why we need to fund it."
+                        :rows="6"
+                        :max-rows="6"
+                        :maxlength="maxDescription">
+          </b-form-textarea>
+        </b-form-group>
+
+        <b-form-group id="roadmap"
+                      label="Financial Roadmap"
+                      label-for="roadmap">
+          <b-form-textarea id="roadmap"
+                        type="text"
+                        v-model="form.roadmap"
+                        required
+                        placeholder="Describe detailed plan for the use of raised funds."
+                        :rows="6"
+                        :max-rows="6"
+                        :maxlength="maxDescription">
+          </b-form-textarea>
+        </b-form-group>
+
+        <h3>Team</h3>
+
+        <b-form-group id="teamMember1"
+                      label="Team Member 1"
+                      label-for="teamMember1">
+          <b-form-input id="teamMember1"
+                        type="url"
+                        v-model="form.teamMember1"
+                        required
+                        placeholder="Copy Paste Linkedin Profile URL">
+          </b-form-input>
+        </b-form-group>
+
+        <b-form-group id="teamMember2"
+                      label="Team Member 2"
+                      label-for="teamMember2">
+          <b-form-input id="teamMember2"
+                        type="url"
+                        v-model="form.teamMember2"
+                        required
+                        placeholder="Copy Paste Linkedin Profile URL">
+          </b-form-input>
+        </b-form-group>
+
+        <b-form-group id="teamMember3"
+                      label="Team Member 3"
+                      label-for="teamMember3">
+          <b-form-input id="teamMember3"
+                        type="url"
+                        v-model="form.teamMember3"
+                        required
+                        placeholder="Copy Paste Linkedin Profile URL">
+          </b-form-input>
+        </b-form-group>
+
+        <h3>Funding</h3>
+
+        <b-form-group id="funding"
+                      label="Funding Goal (in EOS, for 1 year)"
+                      label-for="funding"
+                      description="Your funding goal is the minimum amount you need to run your project">
+          <b-form-input id="funding"  
+                        type="number"
+                        v-model="form.funding"
+                        required
+                        placeholder="Type amount"
+                        :max="maxFund">
+          </b-form-input>
+        </b-form-group>
+
+        <b-form-group id="duration"
+                      label="Voting Duration (Number of days)"
+                      label-for="duration"
+                      description="Maximum 60 days">
+          <b-form-input id="duration"
+                        type="number"
+                        v-model="form.duration"
+                        required
+                        placeholder="Type number of days"
+                        :max="maxDays">
+          </b-form-input>
+        </b-form-group>
+
+        <div class="container" style="margin-bottom:1em; text-align:center;">
+          <b-button type="submit" variant="primary">Submit</b-button>
+          <b-button type="reset" variant="danger">Reset</b-button>
+        </div>
+      </b-form>
+    </div>
+
+
+  </main>
 </template>
 
 <script>
@@ -11,10 +175,47 @@ export default {
   name: 'CreateProject',
   data () {
     return {
-      page: 'CreateProject'
+      maxTitle: '60',
+      maxSubtitle: '140',
+      maxDescription: '2000',
+      maxFund: 1000000000,
+      maxDays: 60,
+      page: 'CreateProject',
+      title: 'Create New Project',
+      form: {
+        categories: null,
+        title: '',
+        subtitle: '',
+        media: '',
+        overview: '',
+        roadmap: '',
+      },
+      categories: [
+        { text: 'Select One', value: null },
+        'Community', 'Education', 'Games', 'Others'
+      ],
+      show: true
+    }
+  },
+  methods: {
+    onSubmit (evt) {
+      evt.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+    onReset (evt) {
+      evt.preventDefault();
+      /* Reset our form values */
+      this.form.category = null;
+      this.form.email = '';
+      this.form.name = '';
+      this.form.checked = [];
+      /* Trick to reset/clear native browser form validation state */
+      this.show = false;
+      this.$nextTick(() => { this.show = true });
     }
   }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
