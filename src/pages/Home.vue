c693fa3b3151bed30010d6706d3180e4f5158ef0
Hugo <hg.campanella@gmail.com>
2018-08-04 18:22:22 +0200

    Home tabs logic

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -45,14 +45,148 @@
         <!-- Popular Projects Tab -->
         <b-tab title="Popular" active>
 
-          <CategoryTabContent></CategoryTabContent>
+          <b-card no-body>
+            <b-tabs card>
+
+              <!-- Hot Tab - Most voted projects on top -->
+              <b-tab title="Hot" active>
+
+                <!-- Projects cards container -->
+                <div class="container">
+                  <div class="row">
+
+                    <!-- Card column -->
+                    <div class="col-md-6 border" v-if="item.category === 'Popular'" v-for="item in projects">
+                      <div class="container">
+                        <div class="row">
+                          <div class="col-md-6">
+                            <img v-bind:src="item.image">
+                          </div>
+                          <div class="col-md-6">
+                            <div class="container fluid">
+                              <div class="row" style="margin-top:1em;">
+                                <div class="col-md-6">
+                                  <p style="color:green;"> {{ item.status }} </p>
+                                </div>
+                                <div class="col-md-6">
+                                  <p> {{ item.count }} </p>
+                                </div>
+                              </div>
+                            </div>
+
+                            <h2>{{ item.name }}</h2>
+                            <p>{{ item.description }}</p>
+
+                            <div class="container fluid">
+                              <div class="row">
+                                <div class="col-md-6">
+                                  <p>{{ item.votes }}</p>
+                                </div>
+                                <div class="col-md-6">
+                                  <span class="badge badge-secondary">{{ item.category }}</span>
+                                </div>
+                              </div>
+                            </div>
+
+                          </div>
+                        </div>
+                      </div>
+                    <!-- End of card column -->
+                    </div>
+
+                  <!-- End or Row -->
+                  </div>
+                </div>
+
+              <!-- Latest Tab - Most Recent projects on top -->
+              </b-tab>
+              <b-tab title="Latest">
+
+                <div class="container">
+                  <div class="row">
+
+                    <!-- Need same cards ordered by date -->
+
+                  </div>
+                </div>
+
+              </b-tab>
+            </b-tabs>
+          </b-card>
 
         <!-- End of Popular Tab -->
         </b-tab>
         <!-- Community Tab -->
         <b-tab title="Community">
 
-          <CategoryTabContent></CategoryTabContent>
+          <b-card no-body>
+            <b-tabs card>
+
+              <!-- Hot Tab - Most voted projects on top -->
+              <b-tab title="Hot" active>
+
+                <!-- Projects cards container -->
+                <div class="container">
+                  <div class="row">
+
+                    <!-- Card column -->
+                    <div class="col-md-6 border" v-if="item.category === 'Community'" v-for="item in projects">
+                      <div class="container">
+                        <div class="row">
+                          <div class="col-md-6">
+                            <img v-bind:src="item.image">
+                          </div>
+                          <div class="col-md-6">
+                            <div class="container fluid">
+                              <div class="row" style="margin-top:1em;">
+                                <div class="col-md-6">
+                                  <p style="color:green;"> {{ item.status }} </p>
+                                </div>
+                                <div class="col-md-6">
+                                  <p> {{ item.count }} </p>
+                                </div>
+                              </div>
+                            </div>
+
+                            <h2>{{ item.name }}</h2>
+                            <p>{{ item.description }}</p>
+
+                            <div class="container fluid">
+                              <div class="row">
+                                <div class="col-md-6">
+                                  <p>{{ item.votes }}</p>
+                                </div>
+                                <div class="col-md-6">
+                                  <span class="badge badge-secondary">{{ item.category }}</span>
+                                </div>
+                              </div>
+                            </div>
+
+                          </div>
+                        </div>
+                      </div>
+                    <!-- End of card column -->
+                    </div>
+
+                  <!-- End or Row -->
+                  </div>
+                </div>
+
+              <!-- Latest Tab - Most Recent projects on top -->
+              </b-tab>
+              <b-tab title="Latest">
+
+                <div class="container">
+                  <div class="row">
+
+                    <!-- Need same cards ordered by date -->
+
+                  </div>
+                </div>
+
+              </b-tab>
+            </b-tabs>
+          </b-card>
 
         <!-- End of Community Tab -->
         </b-tab>
@@ -110,9 +244,24 @@ export default {
       statistic_total_voters: '183,434',
       statistic_total_projects: '143,432',
       statistic_funded_projects: '734,345',
-      statistic_ongoing_projects: '235,289'
+      statistic_ongoing_projects: '235,289',
+      projects: [
+        { image: 'https://vignette.wikia.nocookie.net/undertale-rho/images/5/5f/Placeholder.jpg/revision/latest?cb=20180213155916', status: 'Ongoing', count: '12', name: 'Warhammer', description: 'This is a project description example to illustrate', votes: '432', category: 'Popular' },
+        { image: 'https://vignette.wikia.nocookie.net/undertale-rho/images/5/5f/Placeholder.jpg/revision/latest?cb=20180213155916', status: 'Ongoing', count: '10', name: 'Red Alert', description: 'This is a project description example to illustrate', votes: '432', category: 'Popular' },
+        { image: 'https://vignette.wikia.nocookie.net/undertale-rho/images/5/5f/Placeholder.jpg/revision/latest?cb=20180213155916', status: 'Ongoing', count: '19', name: 'GTA', description: 'This is a project description example to illustrate', votes: '432', category: 'Community' },
+        { image: 'https://vignette.wikia.nocookie.net/undertale-rho/images/5/5f/Placeholder.jpg/revision/latest?cb=20180213155916', status: 'Ongoing', count: '4', name: 'Diablo', description: 'This is a project description example to illustrate', votes: '432', category: 'Popular' },
+        { image: 'https://vignette.wikia.nocookie.net/undertale-rho/images/5/5f/Placeholder.jpg/revision/latest?cb=20180213155916', status: 'Ongoing', count: '19', name: 'Sim City', description: 'This is a project description example to illustrate', votes: '432', category: 'Community' },
+        { image: 'https://vignette.wikia.nocookie.net/undertale-rho/images/5/5f/Placeholder.jpg/revision/latest?cb=20180213155916', status: 'Ongoing', count: '10', name: 'Red Alert', description: 'This is a project description example to illustrate', votes: '432', category: 'Popular' },
+        { image: 'https://vignette.wikia.nocookie.net/undertale-rho/images/5/5f/Placeholder.jpg/revision/latest?cb=20180213155916', status: 'Ongoing', count: '19', name: 'GTA', description: 'This is a project description example to illustrate', votes: '432', category: 'Community' },
+        { image: 'https://vignette.wikia.nocookie.net/undertale-rho/images/5/5f/Placeholder.jpg/revision/latest?cb=20180213155916', status: 'Ongoing', count: '4', name: 'Diablo', description: 'This is a project description example to illustrate', votes: '432', category: 'Popular' },
+        { image: 'https://vignette.wikia.nocookie.net/undertale-rho/images/5/5f/Placeholder.jpg/revision/latest?cb=20180213155916', status: 'Ongoing', count: '19', name: 'Sim City', description: 'This is a project description example to illustrate', votes: '432', category: 'Community' },
+        { image: 'https://vignette.wikia.nocookie.net/undertale-rho/images/5/5f/Placeholder.jpg/revision/latest?cb=20180213155916', status: 'Ongoing', count: '19', name: 'GTA', description: 'This is a project description example to illustrate', votes: '432', category: 'Community' },
+        { image: 'https://vignette.wikia.nocookie.net/undertale-rho/images/5/5f/Placeholder.jpg/revision/latest?cb=20180213155916', status: 'Ongoing', count: '13', name: 'Project Name', description: 'This is a project description example to illustrate', votes: '432', category: 'Popular' },
+        { image: 'https://vignette.wikia.nocookie.net/undertale-rho/images/5/5f/Placeholder.jpg/revision/latest?cb=20180213155916', status: 'Ongoing', count: '19', name: 'GTA', description: 'This is a project description example to illustrate', votes: '432', category: 'Community' }
+      ],
+
     }
-  }
+  },
 }
 </script>
 
