f202b0341b31160fcf277d96e7cf89eae0d373d3
alfredcheuk <alfredcheuk112@hotmail.com>
2018-08-13 10:59:40 +0800

    Not sure if right way to do time sort

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -37,12 +37,12 @@
             <b-tabs card>
 
               <!-- Hot Tab - Most voted projects on top -->
-              <b-tab title="Hot" active>
+              <b-tab title="Hot" active @click="sortBy('votes')">
 
                   <b-card-group deck class="mb-3">
-                    
+
                     <div class="row">
-                      <div class="col-md-4" v-if="item.category === 'Popular'" v-for="item in computedNumbers">
+                      <div class="col-md-4" v-if="item.category === 'Popular'" v-for="item in sortedItems">
 
                         <!-- Project Card -->
                         <b-card   no-body
@@ -57,25 +57,65 @@
                           <b-card-body>
                             <p class="d-inline text-success">{{ item.status }}</p>
                             <span class="badge badge-secondary">{{ item.category }}</span>
-                            <p class="d-inline text-info">{{ item.votes }} votes</p>                        
+                            <p class="d-inline text-info">{{ item.votes }} votes</p>
                             <p class="card-text mt-3">
                               {{ item.description }}
+                            </p>
+                            <p class="card-text mt-3">
+                              {{ item.timestamp }}
                             </p>
                           </b-card-body>
                           <b-card-footer>
                             <button type="button" class="btn btn-secondary">View</button>
-                          </b-card-footer>                    
+                          </b-card-footer>
                         </b-card>
 
                       </div>
-                    </div>   
+                    </div>
 
                   </b-card-group>
-                 
+
               </b-tab>
 
-              <b-tab title="Latest">
-              
+              <b-tab title="Latest" @click="sortBy('timestamp')">
+
+                <b-card-group deck
+                              class="mb-3">
+
+                  <div class="row">
+                    <div class="col-md-4" v-if="item.category === 'Popular'" v-for="item in sortedItems">
+
+                      <!-- Project Card -->
+                      <b-card   no-body
+                                v-bind:img-src="item.image"
+                                img-alt="Image"
+                                img-top
+                                tag="div"
+                                style="max-width: 20rem;"
+                                class="mb-2"
+                      >
+                        <h4 slot="header">{{ item.name }}</h4>
+                        <b-card-body>
+                          <p class="d-inline text-success">{{ item.status }}</p>
+                          <span class="badge badge-secondary">{{ item.category }}</span>
+                          <p class="d-inline text-info">{{ item.votes }} votes</p>
+                          <p class="card-text mt-3">
+                            {{ item.description }}
+                          </p>
+                          <p class="card-text mt-3">
+                            {{ item.timestamp }}
+                          </p>
+                        </b-card-body>
+                        <b-card-footer>
+                          <button type="button" class="btn btn-secondary">View</button>
+                        </b-card-footer>
+                      </b-card>
+
+                    </div>
+                  </div>
+
+                </b-card-group>
+
               </b-tab>
 
             </b-tabs>
@@ -92,11 +132,11 @@
               <!-- Hot Tab - Most voted projects on top -->
               <b-tab title="Hot" active>
 
-                                  <b-card-group deck
+                              <b-card-group deck
                                 class="mb-3">
-                    
+
                     <div class="row">
-                      <div class="col-md-4" v-if="item.category === 'Community'" v-for="item in computedNumbers">
+                      <div class="col-md-4" v-if="item.category === 'Community'" v-for="item in sortedItems">
 
                         <!-- Project Card -->
                         <b-card   no-body
@@ -111,18 +151,21 @@
                           <b-card-body>
                             <p class="d-inline text-success">{{ item.status }}</p>
                             <span class="badge badge-secondary">{{ item.category }}</span>
-                            <p class="d-inline text-info">{{ item.votes }} votes</p>                        
+                            <p class="d-inline text-info">{{ item.votes }} votes</p>
                             <p class="card-text mt-3">
                               {{ item.description }}
+                            </p>
+                            <p class="card-text mt-3">
+                              {{ item.timestamp }}
                             </p>
                           </b-card-body>
                           <b-card-footer>
                             <button type="button" class="btn btn-secondary">View</button>
-                          </b-card-footer>                    
+                          </b-card-footer>
                         </b-card>
 
                       </div>
-                    </div>   
+                    </div>
 
                   </b-card-group>
 
@@ -131,13 +174,42 @@
               </b-tab>
               <b-tab title="Latest">
 
-                <div class="container">
+                <b-card-group deck
+                              class="mb-3">
+
                   <div class="row">
+                    <div class="col-md-4" v-if="item.category === 'Community'" v-for="item in sortedItems">
 
-                    <!-- Need same cards ordered by date -->
+                      <!-- Project Card -->
+                      <b-card   no-body
+                                v-bind:img-src="item.image"
+                                img-alt="Image"
+                                img-top
+                                tag="div"
+                                style="max-width: 20rem;"
+                                class="mb-2"
+                      >
+                        <h4 slot="header">{{ item.name }}</h4>
+                        <b-card-body>
+                          <p class="d-inline text-success">{{ item.status }}</p>
+                          <span class="badge badge-secondary">{{ item.category }}</span>
+                          <p class="d-inline text-info">{{ item.votes }} votes</p>
+                          <p class="card-text mt-3">
+                            {{ item.description }}
+                          </p>
+                          <p class="card-text mt-3">
+                            {{ item.timestamp }}
+                          </p>
+                        </b-card-body>
+                        <b-card-footer>
+                          <button type="button" class="btn btn-secondary">View</button>
+                        </b-card-footer>
+                      </b-card>
 
+                    </div>
                   </div>
-                </div>
+
+                </b-card-group>
 
               </b-tab>
             </b-tabs>
@@ -186,6 +258,7 @@
 
 <script>
 import CategoryTabContent from '../components/CategoryTabContent.vue'
+import { parseDate } from '../utils/parseDate'
 
 export default {
   name: 'Home',
@@ -202,27 +275,34 @@ export default {
         'Funded Projects': '734,345',
         'Ongoing Projects': '235,289'
       },
+      sortProp: 'votes',
       projects: [
-        { image: 'https://picsum.photos/600/300/?image=25', status: 'Ongoing', name: 'Warhammer', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', votes: '432', category: 'Popular' },
-        { image: 'https://picsum.photos/600/300/?image=25', status: 'Ongoing', name: 'Red Alert', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', votes: '345', category: 'Popular' },
-        { image: 'https://picsum.photos/600/300/?image=25', status: 'Ongoing', name: 'GTA', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', votes: '256', category: 'Community' },
-        { image: 'https://picsum.photos/600/300/?image=25', status: 'Ongoing', name: 'Diablo', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', votes: '846', category: 'Popular' },
-        { image: 'https://picsum.photos/600/300/?image=25', status: 'Ongoing', name: 'Sim City', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', votes: '176', category: 'Community' },
-        { image: 'https://picsum.photos/600/300/?image=25', status: 'Ongoing', name: 'Red Alert', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', votes: '431', category: 'Popular' },
-        { image: 'https://picsum.photos/600/300/?image=25', status: 'Ongoing', name: 'GTA', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', votes: '112', category: 'Community' },
-        { image: 'https://picsum.photos/600/300/?image=25', status: 'Ongoing', name: 'Diablo', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', votes: '543', category: 'Popular' },
-        { image: 'https://picsum.photos/600/300/?image=25', status: 'Ongoing', name: 'Sim City', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', votes: '165', category: 'Community' },
-        { image: 'https://picsum.photos/600/300/?image=25', status: 'Ongoing', name: 'GTA', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', votes: '143', category: 'Community' },
-        { image: 'https://picsum.photos/600/300/?image=25', status: 'Ongoing', name: 'Pod', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', votes: '431', category: 'Popular' },
-        { image: 'https://picsum.photos/600/300/?image=25', status: 'Ongoing', name: 'GTA', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', votes: '943', category: 'Community' }
+        { image: 'https://picsum.photos/600/300/?image=25', status: 'Ongoing', name: 'Warhammer', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', votes: '432', category: 'Popular', timestamp: parseDate('2018-02-08 09+07:00') },
+        { image: 'https://picsum.photos/600/300/?image=25', status: 'Ongoing', name: 'Red Alert', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', votes: '345', category: 'Popular', timestamp: parseDate('2018-02-18 09+07:00') },
+        { image: 'https://picsum.photos/600/300/?image=25', status: 'Ongoing', name: 'GTA', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', votes: '256', category: 'Community', timestamp: parseDate('2018-04-08 09+07:00') },
+        { image: 'https://picsum.photos/600/300/?image=25', status: 'Ongoing', name: 'Diablo', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', votes: '846', category: 'Popular', timestamp: parseDate('2018-05-28 09+07:00') },
+        { image: 'https://picsum.photos/600/300/?image=25', status: 'Ongoing', name: 'Sim City', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', votes: '176', category: 'Community', timestamp: parseDate('2018-01-04 09+07:00') },
+        { image: 'https://picsum.photos/600/300/?image=25', status: 'Ongoing', name: 'Red Alert', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', votes: '431', category: 'Popular', timestamp: parseDate('2018-07-31 09+07:00') },
+        { image: 'https://picsum.photos/600/300/?image=25', status: 'Ongoing', name: 'GTA', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', votes: '112', category: 'Community', timestamp: parseDate('2018-12-08 09+07:00') },
+        { image: 'https://picsum.photos/600/300/?image=25', status: 'Ongoing', name: 'Diablo', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', votes: '543', category: 'Popular', timestamp: parseDate('2018-03-09 09+07:00') },
+        { image: 'https://picsum.photos/600/300/?image=25', status: 'Ongoing', name: 'Sim City', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', votes: '165', category: 'Community', timestamp: parseDate('2018-02-08 09+07:00') },
+        { image: 'https://picsum.photos/600/300/?image=25', status: 'Ongoing', name: 'GTA', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', votes: '143', category: 'Community', timestamp: parseDate('2018-10-02 09+07:00') },
+        { image: 'https://picsum.photos/600/300/?image=25', status: 'Ongoing', name: 'Pod', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', votes: '431', category: 'Popular', timestamp: parseDate('2018-07-07 09+07:00') },
+        { image: 'https://picsum.photos/600/300/?image=25', status: 'Ongoing', name: 'GTA', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', votes: '943', category: 'Community', timestamp: parseDate('2018-08-12 09+07:00') }
       ],
     }
   },
   computed: {
-    computedNumbers() {
-      return this.projects.sort(function(a, b) {
-        return b.votes - a.votes;
+    sortedItems() {
+      const self = this;
+      return self.projects.sort(function(a, b) {
+        return b[self.sortProp] - a[self.sortProp];
       });
+    }
+  },
+  methods: {
+    sortBy(prop) {
+      this.sortProp = prop;
     }
   }
 }
---
+++ src/utils/parseDate.js
@@ -0,0 +1,5 @@
+import moment from 'moment'
+
+export function parseDate(dateString) {
+  return moment(dateString);
+}
