957c21d40c89c431b5fdb698705b7b07295debe4
Syed Jafri <syed.jafri2@ucalgary.ca>
2018-09-03 21:36:05 -0600

    Create Project

--- src/pages/CreateProject.vue
+++ src/pages/CreateProject.vue
@@ -8,7 +8,7 @@
           </h1>
       </div>
     <!-- End of jumbotron -->
-    </div>
+    </div>x
 
     <!-- Project Creation Form -->
     <div class="container">
@@ -16,13 +16,24 @@
 
         <h3>Main information</h3>
 
+        <b-form-group id="proposer"
+                      label="Proposer (Linked to Scatter Identity)"
+                      label-for="proposer">
+          <b-form-input id="proposer"
+                        type="text"
+                        v-model="form.proposer"
+                        required
+                        placeholder="Proposer">
+          </b-form-input>
+        </b-form-group>
+
         <b-form-group id="categories"
-                      label="Select Project Category"
+                      label="Select Committee"
                       label-for="categories">
           <b-form-select id="categories"
-                        :options="categories"
+                        :options="committees"
                         required
-                        v-model="form.categories">
+                        v-model="form.committee">
           </b-form-select>
         </b-form-group>
 
@@ -59,7 +70,7 @@
                       label-for="media">
           <b-form-input id="media"
                         type="url"
-                        v-model="form.media"
+                        v-model="form.project_img_url"
                         required
                         placeholder="Copy Paste an Image or Video URL to illustrate project">
           </b-form-input>
@@ -67,18 +78,18 @@
 
         <h3>Project Description</h3>
 
-        <b-form-group id="overview"
-                      label="Project Overview"
-                      label-for="overview"
+        <b-form-group id="description"
+                      label="Project description"
+                      label-for="description"
                       description="Maximum 5000 characters">
-          <b-form-textarea id="overview"
+          <b-form-textarea id="description"
                         type="text"
-                        v-model="form.overview"
+                        v-model="form.description"
                         required
                         placeholder="Describe your project to the community, your story will help us to understand why you started this project and why we need to fund it."
                         :rows="6"
                         :max-rows="6"
-                        :maxlength="maxOverview">
+                        :maxlength="maxdescription">
           </b-form-textarea>
         </b-form-group>
 
@@ -104,20 +115,20 @@
                       label-for="teamMemberInitial">
           <b-form-input id="teamMemberInitial"
                         type="url"
-                        v-model="form.teamMemberInitial"
+                        v-model="teamMemberInitial.url"
                         required
                         placeholder="Copy Paste Linkedin Profile URL">
           </b-form-input>
         </b-form-group>
         <!-- Add remove Team Member section click on button to add Team member form group -->
-        <b-form-group v-for="(row, index) in rows"
+        <b-form-group v-for="(member, index) in form.members"
                       :key="index"
                       id="teamMember"
                       label="Team Member"
                       label-for="teamMember">
           <b-form-input id="teamMember"
                         type="url"
-                        v-model="row.url"
+                        v-model="member.url"
                         required
                         placeholder="Copy Paste Linkedin Profile URL">
           </b-form-input>
@@ -140,7 +151,7 @@
                       description="Your funding goal is the minimum amount you need to run your project">
           <b-form-input id="funding"
                         type="number"
-                        v-model="form.funding"
+                        v-model="form.funding_goal"
                         required
                         placeholder="Type amount"
                         :max="maxFund"
@@ -151,7 +162,7 @@
         <b-form-group id="duration"
                       label="Project Duration (Number of days)"
                       label-for="duration"
-                      description="Maximum 180 days">
+                      description="Maximum 60 days">
           <b-form-input id="duration"
                         type="number"
                         v-model="form.duration"
@@ -172,45 +183,65 @@
 </template>
 
 <script>
+import EOS from 'eosjs'
+import { mapState, mapGetters } from 'vuex'
+
 export default {
   name: 'CreateProject',
   data () {
     return {
       maxTitle: '256',
       maxSummary: '400',
-      maxOverview: '5000',
+      maxdescription: '5000',
       maxRoadmap: '2000',
       maxFund: 1000000000,
       minFund: 1,
       maxDays: 60,
       form: {
-        categories: null,
-        title: '',
-        summary: '',
-        media: '',
-        teamMemberInitial: '',
-        overview: '',
-        roadmap: '',
-        funding: '',
-        duration: ''
+        committee: null,
+        subcategory: null,
+        title: null,
+        summary: null,
+        project_img_url: null,
+        description: null,
+        roadmap: null,
+        duration: null,
+        members: [],
+        funding_goal: null
       },
-      categories: [
+      teamMemberInitial: {
+        url: ''
+      },
+      committees: [
         { text: 'Select One', value: null },
         'Community', 'Education', 'Games', 'Others'
       ],
       show: true,
-      rows: []
+      success: null,
+      error: null
     }
   },
+
+  computed: {
+    ...mapState({
+      scatter: state => state.scatter.scatter,
+      defaultNetwork: state => state.scatter.defaultNetwork
+    }),
+
+    ...mapGetters({
+      scatterAccount: 'api/GET_SCATTER_ACCOUNT'
+    })
+  },
+
   methods: {
     // Dynamic From for Team members
     addRow: function () {
-      this.rows.push({
-        teamMember: ''
+      this.form.members.push({
+        url: ''
       })
     },
     removeElement: function (index) {
-      this.rows.splice(index, 1)
+      this.form.members.splice(index, 1)
     },
     setFilename: function (event, row) {
       var file = event.target.files[0]
@@ -218,7 +249,40 @@ export default {
     },
     onSubmit (evt) {
       evt.preventDefault()
-      alert(JSON.stringify(this.form))
+      
+      // Clone object
+      let form = Object.assign({}, this.form)
+
+      // Update members
+      form.members = ([this.teamMemberInitial, ...form.members]).map(member => member.url)
+
+      // Check that acc exists
+      if (!this.scatterAccount) {
+        alert('link scatter identity')
+        return
+      }
+
+      // Initialize eosjs
+      let eosjs = this.scatter.eos(this.network, EOS)
+
+      // Send TX
+      eosjs.transaction({
+        actions: [{
+          account: 'eosio.wps',
+          name: 'regproposal',
+          authorization: [{
+            actor: this.scatterAccount.name,
+            permission: this.scatterAccount.authority
+          }],
+          data: form
+        }]
+      }).then(success => {
+        this.success = success
+        console.log(success)
+      }).catch(error => {
+        this.error = error
+        console.log(error)
+      })
     },
     onReset (evt) {
       evt.preventDefault()
@@ -227,9 +291,9 @@ export default {
       this.form.category = null
       this.form.title = ''
       this.form.summary = ''
-      this.form.media = ''
+      this.form.project_img_url = ''
       this.form.teamMemberInitial = ''
-      this.form.overview = ''
+      this.form.description = ''
       this.form.roadmap = ''
       this.form.funding = ''
       this.form.duration = ''
